45f943160ee527dea638af36e6a45e3095c88f6c
ibkrob <102308006+ibkrob@users.noreply.github.com>
2022-07-10 17:01:05 +0200

    timeslider

--- assets/js/studyarea.js
+++ assets/js/studyarea.js
@@ -1,69 +1,43 @@
-/* Bike Trail Tirol Beispiel */
+/*  Beispiel */
+var map = L.map('map').setView([45.4518, 7.6025], 7);
 
-let muenchen = {
-    lat: 48.137222,
-    lng: 11.575556,
-    zoom: 15
-};
-
-// WMTS Hintergrundlayer der eGrundkarte Tirol definieren
-const eGrundkarteTirol = {
-    sommer: L.tileLayer(
-        "http://wmts.kartetirol.at/gdi_summer/{z}/{x}/{y}.png", {
-            attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
-        }
-    ),
-    winter: L.tileLayer(
-        "http://wmts.kartetirol.at/gdi_winter/{z}/{x}/{y}.png", {
-            attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
-        }
-    ),
-    ortho: L.tileLayer(
-        "http://wmts.kartetirol.at/gdi_ortho/{z}/{x}/{y}.png", {
-            attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
-        }
-    ),
-    nomenklatur: L.tileLayer(
-        "http://wmts.kartetirol.at/gdi_nomenklatur/{z}/{x}/{y}.png", {
-            attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`,
-            pane: "overlayPane",
-        }
-    )
-}
-
-// eGrundkarte Tirol Sommer als Startlayer
-// Hintergrundlayer Satellitenbild
-let startLayer = L.tileLayer.provider("Esri.WorldImagery")
-
-// Overlays Objekt für den GPX Track Layer
-let overlays = {
-    gpx: L.featureGroup()
-};
-
-// Karte initialisieren
-let map = L.map("map", {
-    center: [muenchen.lat, muenchen.lng],
-    zoom: muenchen.zoom,
-    layers: [
-        startLayer
-    ],
-});
-
-
-// Layer control mit WMTS Hintergründen und Overlay
-let layerControl = L.control.layers({
-    "summer": startLayer,
-    "winter": eGrundkarteTirol.winter,
-    "orthophoto": eGrundkarteTirol.ortho,
-    "surface": L.tileLayer.provider("BasemapAT.surface"),
-    "terrain": L.tileLayer.provider("BasemapAT.terrain"),
-    "orthophoto with names": L.layerGroup([
-        eGrundkarteTirol.ortho,
-        eGrundkarteTirol.nomenklatur,
-    ])
-}, {
-    "Stadtfuehrung-route": overlays.gpx,
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+// L.control.timelineSlider({
+//     timelineItems: ["Day 1", "The Next Day", "Amazing Event", "1776", "12/22/63", "1984"],
+//     extraChangeMapParams: {greeting: "Hello World!"}, 
+//     changeMap: changeMapFunction })
+// .addTo(mymap);
+
+L.marker([44.1736000,5.2787900]).addTo(map)
+    .bindPopup('Mont Ventoux im Jahr 1336')
+    .openPopup();
+
+ L.marker([44.841793,5.552223]).addTo(map)
+    .bindPopup('Erstbesteigung des Mont Aiguille im Jahr 1492')
+    .openPopup();
+
+L.marker([45.89345978,7.25323710]).addTo(map)
+    .bindPopup('Im Jahr 1779: Murith bestieg den Mont Vélan (3731 m ü NN.)')
+    .openPopup();
+    
+L.marker([45.83089911 ,  6.86511039,]).addTo(map)
+    .bindPopup('Erstbesteigung des Mont Blanc')
+    .openPopup();
+
+L.marker([46.5367, 7.9626]).addTo(map)
+    .bindPopup('1811: Erstbesteigung der Jungfrau (4158 m. ü. NN.)    ')
+    .openPopup();
+
+L.marker([46.5899, 8.1182,]).addTo(map)
+    .bindPopup('Die Goldenen Jahre des Alpinismus    ')
+    .openPopup();
+
+L.marker([45.004722, 6.308611,]).addTo(map)
+    .bindPopup('Die Silbernen Jahre des Alpinismus    ')
+    .openPopup();
 
 
 // Maßstab hinzu
@@ -82,107 +56,107 @@ let miniMap = new L.Control.MiniMap(
     }
 ).addTo(map);
 
-// GPX Track Layer beim Laden anzeigen
-overlays.gpx.addTo(map);
+// // GPX Track Layer beim Laden anzeigen
+// overlays.gpx.addTo(map);
 
-// GPX Track Layer implementieren
-let gpxTrack = new L.GPX("./data/stadtfuehrung.gpx", {
-    async: true,
-    marker_options: {
-        startIconUrl: 'icons/start.png',
-        endIconUrl: 'icons/mountain.png',
-        shadowUrl: null,
-        iconSize: [32, 37],
-        iconAnchor: [16, 37],
-    },
-    polyline_options: {
-        color: "black",
-        dashArray: [5, 4],
-    }
+// // GPX Track Layer implementieren
+// let gpxTrack = new L.GPX("./data/stadtfuehrung.gpx", {
+//     async: true,
+//     marker_options: {
+//         startIconUrl: 'icons/start.png',
+//         endIconUrl: 'icons/mountain.png',
+//         shadowUrl: null,
+//         iconSize: [32, 37],
+//         iconAnchor: [16, 37],
+//     },
+//     polyline_options: {
+//         color: "black",
+//         dashArray: [5, 4],
+//     }
 
-}).addTo(overlays.gpx);
-
-
-
-gpxTrack.on("loaded", function (evt) {
-    //console.log ("loaded gpx event: ", evt);
-    map.fitBounds(evt.target.getBounds())
-
-
-    let gpxLayer = evt.target;
-    map.fitBounds(gpxLayer.getBounds());
+// }).addTo(overlays.gpx);
 
 
 
-    new L.GPX(gpx, {
-        async: true
-    }).on('loaded', function (e) {
-        map.fitBounds(e.target.getBounds());
-    }).addTo(map);
+// gpxTrack.on("loaded", function (evt) {
+//     //console.log ("loaded gpx event: ", evt);
+//     map.fitBounds(evt.target.getBounds())
 
 
-    let popup = `
-<h1> Live-Webcam of Hintereisferner: </h1>
-<ul>
-<img src="https://www.foto-webcam.eu/webcam/hintereisferner1/current/180.jpg" href="https://www.foto-webcam.eu/webcam/hintereisferner1/" style="width:170px; border:2px solid silver;" alt="Webcam">
-    <br>
-    <h2> Stadtführung Hard-Facts: </h2>
-    <li>Distance: ${gpxLayer.get_distance().toFixed()/1000} Kilometers </li>
-    <li>Highest Point: ${gpxLayer.get_elevation_max().toFixed()} m. a. Z.</li>
-    <li>Lowest Point: ${gpxLayer.get_elevation_min().toFixed()} m. a. Z.</li>
-    <br>
-    <li>Meters Uphill: ${gpxLayer.get_elevation_gain().toFixed()} Meters </li>
-    <li>Meters Downhill: ${gpxLayer.get_elevation_loss().toFixed()} Meters </li>
-
-</ul>
-`;
-
-
-    // Print
-    L.control.bigImage({
-        position: 'topleft'
-    }).addTo(map);
+//     let gpxLayer = evt.target;
+//     map.fitBounds(gpxLayer.getBounds());
 
 
 
-    // Rainviewer
-    L.control.rainviewer({
-        position: 'bottomleft',
-        nextButtonText: '>',
-        playStopButtonText: 'Play/Stop',
-        prevButtonText: '<',
-        positionSliderLabelText: "Hour:",
-        opacitySliderLabelText: "Opacity:",
-        animationInterval: 500,
-        opacity: 0.5
-    }).addTo(map);
+//     new L.GPX(gpx, {
+//         async: true
+//     }).on('loaded', function (e) {
+//         map.fitBounds(e.target.getBounds());
+//     }).addTo(map);
 
-    gpxLayer.bindPopup(popup);
-});
 
-let elevationControl = L.control.elevation({
-    time: false,
-    theme: "trekking",
-    elevationDIV: "#profile",
-    height: 300,
-    downloadLink: 'link',
-    url: "./data/stadtfuehrung.gpx",
-    options: {
-        preferCanvas: true,
-        collapsed: false,
-        detached: true,
-        useLeafletMarker: true,
-        summary: 'points',
-        hotline: false,
-    }
+//     let popup = `
+// <h1> Live-Webcam of Hintereisferner: </h1>
+// <ul>
+// <img src="https://www.foto-webcam.eu/webcam/hintereisferner1/current/180.jpg" href="https://www.foto-webcam.eu/webcam/hintereisferner1/" style="width:170px; border:2px solid silver;" alt="Webcam">
+//     <br>
+//     <h2> Stadtführung Hard-Facts: </h2>
+//     <li>Distance: ${gpxLayer.get_distance().toFixed()/1000} Kilometers </li>
+//     <li>Highest Point: ${gpxLayer.get_elevation_max().toFixed()} m. a. Z.</li>
+//     <li>Lowest Point: ${gpxLayer.get_elevation_min().toFixed()} m. a. Z.</li>
+//     <br>
+//     <li>Meters Uphill: ${gpxLayer.get_elevation_gain().toFixed()} Meters </li>
+//     <li>Meters Downhill: ${gpxLayer.get_elevation_loss().toFixed()} Meters </li>
+
+// </ul>
+// `;
+
+
+//     // Print
+//     L.control.bigImage({
+//         position: 'topleft'
+//     }).addTo(map);
 
 
 
-}).addTo(map);
+//     // Rainviewer
+//     L.control.rainviewer({
+//         position: 'bottomleft',
+//         nextButtonText: '>',
+//         playStopButtonText: 'Play/Stop',
+//         prevButtonText: '<',
+//         positionSliderLabelText: "Hour:",
+//         opacitySliderLabelText: "Opacity:",
+//         animationInterval: 500,
+//         opacity: 0.5
+//     }).addTo(map);
+
+//     gpxLayer.bindPopup(popup);
+// });
+
+// let elevationControl = L.control.elevation({
+//     time: false,
+//     theme: "trekking",
+//     elevationDIV: "#profile",
+//     height: 300,
+//     downloadLink: 'link',
+//     url: "./data/stadtfuehrung.gpx",
+//     options: {
+//         preferCanvas: true,
+//         collapsed: false,
+//         detached: true,
+//         useLeafletMarker: true,
+//         summary: 'points',
+//         hotline: false,
+//     }
 
 
 
-gpxTrack.on("addline", function (evt) {
+// }).addTo(map);
 
-    elevationControl.addData(evt.line);
-});
+
+
+// gpxTrack.on("addline", function (evt) {
+
+//     elevationControl.addData(evt.line);
+// });
